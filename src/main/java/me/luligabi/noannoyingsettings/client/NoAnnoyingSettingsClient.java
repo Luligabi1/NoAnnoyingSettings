5bec08989637d3cc7acb54b15c32d50a76ddd6bf
Luligabi1 <luligabi111@gmail.com>
2021-03-07 22:28:02 -0300

    Added auto-jump disabling and configuration file.

--- src/main/java/me/luligabi/noannoyingsettings/client/NoAnnoyingSettingsClient.java
+++ src/main/java/me/luligabi/noannoyingsettings/client/NoAnnoyingSettingsClient.java
@@ -10,10 +10,26 @@ import net.minecraft.sound.SoundCategory;
 @Environment(EnvType.CLIENT)
 public class NoAnnoyingSettingsClient implements ClientModInitializer {
 
+    SimpleConfig config = SimpleConfig.of("noannoyingsettings").provider(this::provider).request();
+
+    private String provider(String filename) {
+        return "# No Annoying Settings Configuration File\n\n" +
+
+                "# Keep in mind this will only prevent the mod of changing the settings every start. If the config is already set, it will remain the same." +
+
+                "disableAutoJump=true\n" +
+                "disableMusic=true";
+    }
     @Override
     public void onInitializeClient() {
         ClientLifecycleEvents.CLIENT_STARTED.register(minecraftClient -> {
-            MinecraftClient.getInstance().options.setSoundVolume(SoundCategory.MUSIC, 0);
+            if(config.getOrDefault("disableAutoJump", true)) {
+                MinecraftClient.getInstance().options.autoJump = false;
+            }
+            if(config.getOrDefault("disableMusic", true)) {
+                MinecraftClient.getInstance().options.setSoundVolume(SoundCategory.MUSIC, 0);
+            }
+            MinecraftClient.getInstance().options.skipMultiplayerWarning = true;
         });
     }
 }
